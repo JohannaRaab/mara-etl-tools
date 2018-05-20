d8246051ae6587bc65656b6d62482aade1d0feb0
Jan Katins <jasc@gmx.net>
2018-05-20 15:03:05 +0200

    Use mara-config

--- etl_tools/config.py
+++ etl_tools/config.py
@@ -2,17 +2,21 @@
 
 import datetime
 
+from mara_config import declare_config
 
+@declare_config()
 def first_date_in_time_dimensions() -> datetime.date:
     """The first date that should appear in time dimensions"""
     return datetime.date.today() - datetime.timedelta(days=365)
 
 
+@declare_config()
 def last_date_in_time_dimensions() -> datetime.date:
     """The last date that should appear in time dimensions"""
     return datetime.date.today()
 
 
+@declare_config()
 def number_of_chunks() -> int:
     """Big tables and computations are split into this many chunks"""
     return 7
--- setup.py
+++ setup.py
@@ -7,10 +7,12 @@ setup(
     description='Utilities for creating ETL pipelines with mara',
 
     install_requires=[
+        'mara-config>=0.1'
         'data_integration>=1.0.0',
     ],
 
     dependency_links=[
+        'git+https://github.com/mara/mara-config.git@0.1#egg=mara-config-0.1',
         'git+https://github.com/mara/data-integration.git@1.0.0#egg=data-integration-1.0.0'
     ],
 
