31d5e2fbf322f48034168a5dee4d32261df59068
Alexandru Pisarenco <alexandru.pisarenco@kfzteile24.de>
2018-05-31 14:01:04 +0200

    Speed-up parallel indexing of dataset attributes

--- etl_tools/initialize_utils/data_sets.sql
+++ etl_tools/initialize_utils/data_sets.sql
@@ -2,29 +2,40 @@
 
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
--- creates a table optimized for the auto-completion of data set attributes
-CREATE OR REPLACE FUNCTION util.create_data_set_attributes_table(schema_name TEXT, table_name TEXT)
-  RETURNS VOID AS $$
-DECLARE column_name TEXT;
+-- gets attributes from data set (text fields)
+CREATE OR REPLACE FUNCTION util.get_data_set_attributes(param_schema_name TEXT, param_table_name TEXT)
+  RETURNS SETOF TEXT AS $$
 BEGIN
-  EXECUTE 'DROP TABLE IF EXISTS ' || schema_name || '.' || table_name || '_attributes';
-  EXECUTE 'CREATE TABLE ' || schema_name || '.' || table_name ||
+  RETURN QUERY
+  SELECT
+    column_name
+  FROM information_schema.columns
+  WHERE table_schema = param_schema_name
+        AND table_name = param_table_name
+        AND data_type IN ('text', 'varchar');
+END $$ LANGUAGE plpgsql;
+
+-- creates a table optimized for the auto-completion of data set attributes
+CREATE OR REPLACE FUNCTION util.create_data_set_attributes_table(param_schema_name TEXT, param_table_name TEXT)
+  RETURNS VOID AS $$
+DECLARE v_column_name TEXT;
+BEGIN
+  EXECUTE 'DROP TABLE IF EXISTS ' || param_schema_name || '.' || param_table_name || '_attributes';
+  EXECUTE 'CREATE TABLE ' || param_schema_name || '.' || param_table_name ||
           '_attributes (attribute TEXT NOT NULL, value TEXT NOT NULL);';
 
-  FOR column_name IN EXECUTE 'SELECT column_name FROM information_schema.columns WHERE table_schema = ''' ||
-                             schema_name || ''' AND table_name   = ''' || table_name ||
-                             ''' AND data_type IN (''text'',''varchar'')' LOOP
-    EXECUTE 'INSERT INTO ' || schema_name || '.' || table_name || '_attributes ' ||
-            'SELECT DISTINCT ''' || column_name || ''', "' || column_name ||
-            '" FROM ' || schema_name || '.' || table_name ||
-            ' WHERE "' || column_name || '" IS NOT NULL ORDER BY "' || column_name || '"';
+  FOR v_column_name IN util.get_data_set_attributes(param_schema_name, param_table_name) LOOP
+    EXECUTE 'INSERT INTO ' || param_schema_name || '.' || param_table_name || '_attributes ' ||
+            'SELECT DISTINCT ''' || v_column_name || ''', "' || v_column_name ||
+            '" FROM ' || param_schema_name || '.' || param_table_name ||
+            ' WHERE "' || v_column_name || '" IS NOT NULL ORDER BY "' || v_column_name || '"';
   END LOOP;
 
-  EXECUTE 'CREATE INDEX ' || table_name || '_attributes__attribute ON ' ||
-          schema_name || '.' || table_name || '_attributes (attribute)';
+  EXECUTE 'CREATE INDEX ' || param_table_name || '_attributes__attribute ON ' ||
+          param_schema_name || '.' || param_table_name || '_attributes (attribute)';
 
-  EXECUTE 'CREATE INDEX ' || table_name || '_attributes__value ON ' ||
-          schema_name || '.' || table_name || '_attributes USING GIN (value gin_trgm_ops)';
+  EXECUTE 'CREATE INDEX ' || param_table_name || '_attributes__value ON ' ||
+          param_schema_name || '.' || param_table_name || '_attributes USING GIN (value gin_trgm_ops)';
 END;
 $$ LANGUAGE plpgsql;
 
@@ -63,15 +74,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- parallized after function
-CREATE OR REPLACE FUNCTION util.get_data_set_attributes(schema_name TEXT, table_name TEXT)
-  RETURNS SETOF TEXT AS $$
-BEGIN
-  return QUERY
-  execute 'SELECT DISTINCT attribute FROM ' || schema_name || '.' || table_name || '_attributes';
-END $$ LANGUAGE plpgsql;
-
-
+-- parallized indexing
 CREATE OR REPLACE FUNCTION util.index_data_set_attributes_table(schema_name TEXT, table_name TEXT, attribute TEXT)
   RETURNS void AS $$
 BEGIN
